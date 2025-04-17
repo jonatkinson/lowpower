8daf4f5a8df0942aea3824f116f82a76db2e70e2
Jon Atkinson <jon@jonatkinson.co.uk>
2025-04-17 17:52:14 +0100

    feat: update installer to include highpower script
    - Update install.sh to copy both lowpower and highpower scripts
    - Make both scripts executable during installation
    - Improve installation messages

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Installer for lowpower script
+# Installer for power management scripts
 
 # Create directory if it doesn't exist
 mkdir -p ~/.local/bin
 
-# Copy script to user bin directory
+# Copy scripts to user bin directory
 cp ./lowpower ~/.local/bin/
+cp ./highpower ~/.local/bin/
 
-# Make sure it's executable
+# Make sure they're executable
 chmod +x ~/.local/bin/lowpower
+chmod +x ~/.local/bin/highpower
 
-echo "Low power mode script installed to ~/.local/bin/lowpower"
+echo "Power management scripts installed to ~/.local/bin/"
+echo "- lowpower: Reduces power consumption"
+echo "- highpower: Maximizes performance"
 echo "Make sure ~/.local/bin is in your PATH"
 
 exit 0
